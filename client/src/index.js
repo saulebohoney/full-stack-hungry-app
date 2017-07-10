f562e12fb89952a9fc2a574c1fa9c3f9a53aa293
Saule BN <beyonce@thinkful.com>
2017-07-10 15:39:25 -0400

    built the store, removed all App files, added empty actions, reducers, components

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import ReactDOM from 'react-dom';
-import App from './App';
+import HungryList from './components/hungry-list';
 import './index.css';
+import store from '.store';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+  <HungryList />
+  </Provider>,
   document.getElementById('root')
 );

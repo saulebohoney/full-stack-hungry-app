23523f87be5bcb922dd2557356a9e17277f12534
patricewhite <patricewhite2012@gmail.com>
2017-07-11 11:42:48 -0400

    added PUT endpoint

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const path = require('path');
 
 const express = require('express');
@@ -62,14 +64,45 @@ app.post('/api/users', (req, res) => {
     });
 });
 
+//update user information
+app.put('/api/users/:id', (req, res) => {
+  //check if valid id
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    res.status(400).json({message: message});
+  }
+  //which filed(s) can be updated
+  const toUpdate = {};
+  const updateableField = ['nevers'];
+  updateableField.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+  User
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .exec()
+    .then(user => {
+      res.status(204).end();
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+
+});
+
 // Serve the built client
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Unhandled requests which aren't for the API should serve index.html so
 // client-side routing using browserHistory can function
 app.get(/^(?!\/api(\/|$))/, (req, res) => {
-    const index = path.resolve(__dirname, '../client/build', 'index.html');
-    res.sendFile(index);
+  const index = path.resolve(__dirname, '../client/build', 'index.html');
+  res.sendFile(index);
 });
 
 let server;

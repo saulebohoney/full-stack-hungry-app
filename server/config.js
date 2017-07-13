3b59fa1c1a929c84625ede6edb13dfa583d56a65
Saule BN <saulebohoney@gmail.com>
2017-07-13 10:18:02 -0400

    almost done

--- server/config.js
+++ server/config.js
@@ -4,5 +4,5 @@ require('dotenv').config();
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://pj:pass@ds153732.mlab.com:53732/hungerover';
+                      'mongodb://localhost/hungryAPP';
 exports.PORT = process.env.PORT || 3001;

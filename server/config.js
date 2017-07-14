222429f7d89e2d3078f45b860aa0096caa643bcd
patricewhite <patricewhite2012@gmail.com>
2017-07-14 12:43:36 -0400

    test database

--- server/config.js
+++ server/config.js
@@ -7,5 +7,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                       'mongodb://localhost/hungryAPP';
 exports.TEST_DATABASE_URL=process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/test-hungryAPP';
+                      'mongodb://localhost/hungryAPP-test';
 exports.PORT = process.env.PORT || 3001;
